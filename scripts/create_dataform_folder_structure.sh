5ca49717408f7768c2dbc957b03f6469b3952460
smisachsen <stian.mortenssonn.isachsen@entur.org>
2025-09-11 14:02:41 +0200

    Use spaces in dataform folder names in create_dataform_folder_structure.sh (#7)
    * Add backend prefix using git repo name in create_terraform_folder_structure.sh
    
    * Use spaces in dataform folder names in create_dataform_folder_structure.sh

--- scripts/create_dataform_folder_structure.sh
+++ scripts/create_dataform_folder_structure.sh
@@ -9,8 +9,13 @@ fi
 
 TARGET_PATH="$1"
 
-# Create the target directory and includes subfolders
-mkdir -p "$TARGET_PATH/includes/sources" "$TARGET_PATH/includes/intermediate" "$TARGET_PATH/includes/outputs" "$TARGET_PATH/includes/extras"
+
+# Create the target directory and includes subfolders with numbered prefixes and spaces
+mkdir -p \
+  "$TARGET_PATH/includes/01 sources" \
+  "$TARGET_PATH/includes/02 intermediate" \
+  "$TARGET_PATH/includes/03 outputs" \
+  "$TARGET_PATH/includes/04 extras"
 
 # Create workflow_settings.yaml with the specified content in the target path
 cat <<'EOL' > "$TARGET_PATH/workflow_settings.yaml"
@@ -21,8 +26,8 @@ defaultAssertionDataset: dataform_assertions
 dataformCoreVersion: 3.0.0
 EOL
 
-# Write raw_data.sqlx in sources
-cat <<'EOL' > "$TARGET_PATH/includes/sources/raw_data.sqlx"
+ # Write raw_data.sqlx in 01 sources
+cat <<'EOL' > "$TARGET_PATH/includes/01 sources/raw_data.sqlx"
 declare({
     database: 'gcp_project',
     schema: 'gcp_dataset_name',
@@ -30,8 +35,8 @@ declare({
 });
 EOL
 
-# Write transform.sqlx in intermediate
-cat <<'EOL' > "$TARGET_PATH/includes/intermediate/transform.sqlx"
+ # Write transform.sqlx in 02 intermediate
+cat <<'EOL' > "$TARGET_PATH/includes/02 intermediate/transform.sqlx"
 config {
   type: 'table',
   description: 'description'
